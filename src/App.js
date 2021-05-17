a8ae3426e9483ec6745c97ec7f04246427c6baca
tarun9142 <tarunkumar9142@gmail.com>
2021-05-17 17:07:07 +0530

    added redux, and implemented userReducer and userAction to header and app components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Route , Switch} from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -8,38 +9,30 @@ import AuthenticationPage from './pages/authentication/authentication.component'
 
 
 import { auth, createUserProfileDocument } from '../src/firebase/firebase.utils';
-
+import { setCurrentUser } from './redux/user/user.action';
 
 import './App.css';
 
 
 class App extends React.Component {
-  constructor(){
-    super();
-
-    this.state = {
-      currentUser: null
-    }
-  }
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
        if(userAuth){
          const userRef = await createUserProfileDocument(userAuth);
 
          userRef.onSnapshot(snapshot => {
-           this.setState({
-             currentUser: {
+           setCurrentUser({
                id:snapshot.id,
                ...snapshot.data()
-             }
            });
          })
        }
        else{
-         this.setState({currentUser: userAuth});
+         setCurrentUser(userAuth);
        }
     });
   }
@@ -52,7 +45,7 @@ class App extends React.Component {
   render(){
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header/>
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
@@ -67,4 +60,7 @@ class App extends React.Component {
   
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+export default connect(null,mapDispatchToProps)(App);
