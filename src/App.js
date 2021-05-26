5962f6c3f58ffa4709b045cab1f43183741af33b
tarun9142 <tarunkumar9142@gmail.com>
2021-05-26 20:01:13 +0530

    added addItem and RemoveItem functionality to checkout item component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import {Route , Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect'; 
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
 import AuthenticationPage from './pages/authentication/authentication.component';
+import CheckoutPage from './pages/checkoutPage/checkout.component';
 
+import Header from './components/header/header.component';
 
 import { auth, createUserProfileDocument } from '../src/firebase/firebase.utils';
+
 import { setCurrentUser } from './redux/user/user.action';
+
+import { selectCurrentUser } from './redux/user/user.selectors';
 
 import './App.css';
 
@@ -49,7 +54,8 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' render= {() => this.props.currentUser ? (<Redirect to='/'/>) : (<AuthenticationPage/>)} />
+          <Route exact path='/checkout' component={CheckoutPage} />
+          <Route exact path='/signin' render= {() => this.props.currentUser ? (<Redirect to='/'/>) : (<AuthenticationPage/>)} />
   
         </Switch>  
         
@@ -60,9 +66,9 @@ class App extends React.Component {
   
 }
 
-const mapStateToProps = ({user}) => ({
-  currentUser: user.currentUser
-})
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
+});
 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
